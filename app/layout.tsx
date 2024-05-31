fc885f4147a8f66b64bb290d1a4468d25a432841
Andi Dwi Prastyo <2141720046@student.polinema.ac.id>
2024-05-31 20:45:24 +0700

    feat: Change the content

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,14 +10,14 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "andidprastyo | Personal Portfolio",
+  description: "Andi Dwi Prastyo's personal portfolio website.",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,20 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
+        I'm an undergraduate student majoring in{" "}
+        <span className="font-medium">Information Technology</span>, I decided to pursue my
+        passion for applied statistics, data analysis, machine learning and also web development.{" "}
+        My core stack for web development
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+          Next.js, Laravel, TypeScript, Tailwind-CSS, Prisma and OpenSearch
+        </span>. I am also familiar with {" "}
+        <span className="font-medium">
+          Python, Pandas, Scikit-Learn, Microsoft Excel, SQL, Tableau, Power BI, and Google Looker Studio
+        </span> for data analysis and predictive modelling. {" "}
+        I am always looking to
+        learn new technologies. I am currently looking for an{" "}
+        <span className="font-medium">internship</span> in the field of data science or web development.
       </p>
     </motion.section>
   );
